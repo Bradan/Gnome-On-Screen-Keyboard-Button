f62bdac02a6c50cf32eb5436bd53ace1392afa49
Daniel Brall <git@bradan.eu>
2016-04-02 16:49:01 +0200

    UI changes in enable() instead of init()

--- On_Screen_Keyboard_Button@bradan.eu/extension.js
+++ On_Screen_Keyboard_Button@bradan.eu/extension.js
@@ -8,24 +8,27 @@ const Gio = imports.gi.Gio;
 const A11Y_APPLICATIONS_SCHEMA = 'org.gnome.desktop.a11y.applications';
 const SHOW_KEYBOARD = 'screen-keyboard-enabled';
 
-let oskButton;
-let oskA11yApplicationsSettings;
+let _oskButton;
+let _oskButtonEventHandler;
+let _oskA11yApplicationsSettings;
 
 function _toggleKeyboard() {
     if(Main.keyboard._keyboardVisible) {
         Main.keyboard.Hide();
     } else {
         // currently this needs two clicks. I would have to delay the execution otherwise:
-        oskA11yApplicationsSettings.set_boolean(SHOW_KEYBOARD, true);
+        _oskA11yApplicationsSettings.set_boolean(SHOW_KEYBOARD, true);
         Main.keyboard._sync();
         Main.keyboard.Show();
     }
 }
 
 function init() {
-    oskA11yApplicationsSettings = new Gio.Settings({ schema_id: A11Y_APPLICATIONS_SCHEMA });
+    _oskA11yApplicationsSettings = new Gio.Settings({ schema_id: A11Y_APPLICATIONS_SCHEMA });
+}
 
-    oskButton = new St.Bin({ style_class: 'panel-button',
+function enable() {
+    _oskButton = new St.Bin({ style_class: 'panel-button',
                             reactive: true,
                             can_focus: true,
                             x_fill: true,
@@ -34,14 +37,15 @@ function init() {
     let icon = new St.Icon({ icon_name: 'format-text-bold-symbolic',
                             style_class: 'system-status-icon' });
 
-    oskButton.set_child(icon);
-    oskButton.connect('button-press-event', _toggleKeyboard);
-}
+    _oskButton.set_child(icon);
+    _oskButtonEventHandler = _oskButton.connect('button-press-event', _toggleKeyboard);
 
-function enable() {
-    Main.panel._rightBox.insert_child_at_index(oskButton, 0);
+    Main.panel._rightBox.insert_child_at_index(_oskButton, 0);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(oskButton);
+    Main.panel._rightBox.remove_child(_oskButton);
+
+    _oskButton.disconnect(_oskButtonEventHandler);
+    delete _oskButton;
 }
